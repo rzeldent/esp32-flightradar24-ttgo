335a2e4522813fca9789730250a372c8962da01f
Rene Zeldenthuis <rene@zeldenthuis.eu>
2021-12-05 22:11:36 +0100

    Removed logging line

--- src/zimage.cpp
+++ src/zimage.cpp
@@ -1,16 +1,14 @@
 #include <esp32-hal-log.h>
 #include <zimage.h>
 #include <miniz.h>
-#include <esp.h>
 
 unsigned short* z_image_decode(const z_image_data_t *z_image_data)
 {
     auto image = new unsigned short[z_image_data->width * z_image_data->height];
     assert(image);
     ulong uncompressed_len = z_image_data->width * z_image_data->height * sizeof(unsigned short);
-    log_i("Decompressing zimage. Heap: %lu", ESP.getFreeHeap());
     auto status = uncompress((unsigned char*)image, &uncompressed_len, z_image_data->compressed_data, z_image_data->compressed_size);
-    log_i("Decompressed: %u -> %u.", z_image_data->compressed_size, uncompressed_len);
+    log_i("Decompressed image: %u -> %u bytes.", z_image_data->compressed_size, uncompressed_len);
     assert(status == MZ_OK);
     return image;
 }
