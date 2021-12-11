84d6b97e393ea05ce37fe9285c6e1ca06855ac4b
Rene Zeldenthuis <rene@zeldenthuis.eu>
2021-12-11 23:40:49 +0100

    Memory optimization

--- src/zimage.cpp
+++ src/zimage.cpp
@@ -8,7 +8,7 @@ unsigned short* z_image_decode(const z_image_data_t *z_image_data)
     assert(image);
     ulong uncompressed_len = z_image_data->width * z_image_data->height * sizeof(unsigned short);
     auto status = uncompress((unsigned char*)image, &uncompressed_len, z_image_data->compressed_data, z_image_data->compressed_size);
-    log_i("Decompressed image: %u -> %u bytes.", z_image_data->compressed_size, uncompressed_len);
+    log_d("Decompressed image: %u -> %u bytes.", z_image_data->compressed_size, uncompressed_len);
     assert(status == MZ_OK);
     return image;
 }
