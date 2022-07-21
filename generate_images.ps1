ee4b7c7a1c547156d71114be25d8ed117ebbba57
Rene Zeldenthuis <rzeldent@gmail.com>
2022-07-21 16:38:52 +0200

    Created ps1 file

---
+++ generate_images.ps1
@@ -0,0 +1,6 @@
+. python3 -m pip install --upgrade pip setuptools wheel
+. python3 -m pip install Pillow
+
+. python3 ./import/images_to_cpp.py ./images ./include/images.h
+. python3 ./import/images_to_cpp.py ./images/airline_logos ./include/airline_logos.h 40 40
+. python3 ./import/images_to_cpp.py ./images/country_flags ./include/country_flags.h 24 12
