94ac961ad2ee4189db0f867afe57fe67241a9b6a
Rene Zeldenthuis <rene@zeldenthuis.eu>
2022-07-25 23:14:39 +0200

    Refactored zimage

--- include/zimage.h
+++ include/zimage.h
@@ -1,12 +1,20 @@
 #pragma once
 
-// Data is stored as RLE (Run Length Encoding)
+#include <miniz.h>
+
+// Data is stored as GZIP compressed data.
 typedef struct
 {
     const unsigned char *compressed_data;
-    const unsigned int compressed_size;
+    const mz_ulong compressed_size;
     const unsigned short width;
     const unsigned short height;
 } z_image_data_t;
 
-extern unsigned short* z_image_decode(const z_image_data_t *z_image);
+inline unsigned short* z_image_decode(const z_image_data_t *z_image_data)
+{
+    auto image = new unsigned short[z_image_data->width * z_image_data->height];
+    mz_ulong uncompressed_len = z_image_data->width * z_image_data->height * sizeof(unsigned short);
+    auto status = uncompress((unsigned char*)image, &uncompressed_len, z_image_data->compressed_data, z_image_data->compressed_size);
+    return image;
+}
