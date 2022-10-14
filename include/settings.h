4fef04dbcfba8b0e48d0664ee5ff6cf8bfaeb995
Rene Zeldenthuis <rzeldent@gmail.com>
2022-10-14 21:15:49 +0200

    Implemented filters

--- include/settings.h
+++ include/settings.h
@@ -5,7 +5,7 @@
 
 #define WIFI_SSID "FlightRadar"
 #define WIFI_PASSWORD nullptr
-#define CONFIG_VERSION "1.4"
+#define CONFIG_VERSION "1.5"
 
 // One degree is 110.567 km (69.091 miles)
 
@@ -22,6 +22,10 @@
 #define DEFAULT_LOCATION "Amsterdam Schiphol Airport"
 #define DEFAULT_LATITUDE 52.308601
 #define DEFAULT_LONGITUDE 4.76389
+#define DEFAULT_AIR true
+#define DEFAULT_GROUND false
+#define DEFAULT_GLIDERS true
+#define DEFAULT_VEHICLES false
 #define DEFAULT_TIMEZONE "Europe/Amsterdam"
 #define DEFAULT_METRIC true
 // By default take a 10km range
@@ -32,6 +36,10 @@
 //#define DEFAULT_LOCATION "Charles de Gaulle International"
 //#define DEFAULT_LATITUDE 49.012798
 //#define DEFAULT_LONGITUDE 2.55000
+//#define DEFAULT_AIR true
+//#define DEFAULT_GROUND false
+//#define DEFAULT_GLIDERS true
+//#define DEFAULT_VEHICLES false
 //#define DEFAULT_TIMEZONE "Europe/Paris"
 //#define DEFAULT_METRIC true
 // By default take a 10km range
@@ -42,6 +50,10 @@
 //#define DEFAULT_LOCATION "John F Kennedy International"
 //#define DEFAULT_LATITUDE 40.639801
 //#define DEFAULT_LONGITUDE -73.7789
+//#define DEFAULT_AIR true
+//#define DEFAULT_GROUND false
+//#define DEFAULT_GLIDERS true
+//#define DEFAULT_VEHICLES false
 //#define DEFAULT_TIMEZONE "America/New_York"
 //#define DEFAULT_METRIC false
 // By default take a 5mile range
@@ -52,6 +64,9 @@
 //#define DEFAULT_LOCATION "Los Angeles International"
 //#define DEFAULT_LATITUDE 33.942501
 //#define DEFAULT_LONGITUDE -118.407997
+//#define DEFAULT_AIR true
+//#define DEFAULT_GROUND false
+//#define DEFAULT_GLIDERS true
 //#define DEFAULT_TIMEZONE "America/Los_Angeles"
 //#define DEFAULT_METRIC false
 // By default take a 5mile range
